2b7406c307dd2bdc6580c92be5df05bf8edf6779
AntonBoev14 <antonboev421@gmail.com>
2024-11-07 10:07:00 +0300

    Add files via upload

---
+++ pr7.3/pr7.3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(0, "ru");
+    int n;        
+    double S = 0; 
+    int i = 1;    
+
+   
+    std::cout << "Введите натуральное число n (n >= 1): ";
+    std::cin >> n;
+
+    if (n < 1) {
+        std::cout << "Число n должно быть натуральным и больше или равно 1." << std::endl;
+        return 1; 
+    }
+
+    do {
+        S += static_cast<double>(i) / 2; 
+        i++; 
+    } while (i <= n); 
+
+    std::cout << "S = " << S << std::endl;
+
+    return 0;
+}
